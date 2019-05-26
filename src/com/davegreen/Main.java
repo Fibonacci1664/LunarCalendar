3aa0fe26183bd03173139929360b79549646973d
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2019-05-26 12:14:11 +0100

    First commit

---
+++ src/com/davegreen/Main.java
@@ -0,0 +1,32 @@
+package com.davegreen;
+
+import java.util.Calendar;
+import static java.lang.System.out;
+
+class FullMoons
+{                    //milli//min//hour//day
+    static int DAY_IM = 1000 * 60 * 60 * 24;
+    
+    public static void main(String[] args)
+    {
+        Calendar c = Calendar.getInstance();
+        c.set(2004, 0, 7, 15, 40);
+        long day1 = c.getTimeInMillis();
+        
+        for(int x = 0; x < 170; x ++)
+        {
+            day1 += (DAY_IM * 29.52);
+            c.setTimeInMillis(day1);
+            out.println(String.format("Full moon on %tc", c));
+        }
+    }
+    
+}
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+    
+    }
+}
